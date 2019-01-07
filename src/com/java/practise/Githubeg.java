26b7197fd503b3d328bfcdb0ff8e49afaee54f9f
Priyajimin <46382897+Priyajimin@users.noreply.github.com>
2019-01-07 15:20:38 +0530

    Local repo to github
    Local to github

---
+++ src/com/java/practise/Githubeg.java
@@ -0,0 +1,9 @@
+package com.java.practise;
+
+public class Githubeg {
+
+	public static void main(String[] args) {
+		System.out.println("Git hub demo");
+		System.out.println("Git hub updated");
+	}
+}
